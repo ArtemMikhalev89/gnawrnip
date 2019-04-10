7e504df8f8429db360cee00c822ddb0fd739132d
Artem <snoopdogg89@bk.ru>
2019-04-10 15:51:00 +0300

    test1

--- lib/gnawrnip/ext/capybara/session.rb
+++ lib/gnawrnip/ext/capybara/session.rb
@@ -2,43 +2,10 @@ require 'capybara/session'
 
 module Capybara
   class Session
-    SAVE_SCREENSHOT_METHODS = [
-  :body,
-  :html,
-  :source,
-  :current_url,
-  :current_host,
-  :current_path,
-  :execute_script,
-  :evaluate_script,
-  :visit,
-  :refresh,
-  :go_back,
-  :go_forward,
-  :within,
-  :within_element,
-  :within_fieldset,
-  :within_table,
-  :within_frame,
-  :switch_to_frame,
-  :current_window,
-  :windows,
-  :open_new_window,
-  :switch_to_window,
-  :within_window,
-  :window_opened_by,
-  :save_page,
-  :save_and_open_page,
-  :save_screenshot,
-  :save_and_open_screenshot,
-  :reset_session!,
-  :response_headers,
-  :status_code,
-  :current_scope,
-  :assert_current_path,
-  :assert_no_current_path,
-  :has_current_path?,
-  :has_no_current_path?
+    SAVE_SCREENSHOT_METHODS = %i[
+      click_button click_link click_link_or_button click_on
+      fill_in choose check uncheck select unselect attach_file
+      execute_script go_back go_forward switch_to_window visit
 ]
 
 
